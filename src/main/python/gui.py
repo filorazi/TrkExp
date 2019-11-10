d2ee78a3b78311ff9b932b8f2105f97c9e613d84
filorazi <orazifilippo@gmail.com>
2019-11-10 08:20:26 +0100

    added tab in the gui

--- src/main/python/gui.py
+++ src/main/python/gui.py
@@ -6,10 +6,37 @@ class  Gui(QDialog):
         self.originalPalette = QApplication.palette()
         self.label = QLabel("Ciao")
         mainLayout = QGridLayout()
-        mainLayout.addWidget(self.label)
+        tab = QTabWidget()
+        tab.addTab(self.frontPage(),"1")
+        tab.addTab(self.secondPage(),"2")
+        tab.addTab(self.lastPage(),"3")
+        mainLayout.addWidget(tab)
         self.setLayout(mainLayout)
         self.setWindowTitle("Styles")
 
+    def frontPage(self):
+        page = QWidget()
+        label = QLabel("aaaaa")
+        vbox = QVBoxLayout()
+        vbox.addWidget(label)
+        page.setLayout(vbox)
+        return page
+
+    def secondPage(self):
+        page = QWidget()
+        label = QLabel("ccccc")
+        vbox = QVBoxLayout()
+        vbox.addWidget(label)
+        page.setLayout(vbox)
+        return page
+
+    def lastPage(self):
+        page = QWidget()
+        label = QLabel("bbbbb")
+        vbox = QVBoxLayout()
+        vbox.addWidget(label)
+        page.setLayout(vbox)
+        return page
 
     def exec(self):
         self.app.exec_()
